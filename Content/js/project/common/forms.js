81928f3b9af7b4f273ebaea9f6d9cbcc86f1a58c
Thom Duppstadt <tduppstadt@gmail.com>
2014-04-07 11:26:27 -0700

    removed model from forms

--- Content/js/project/common/forms.js
+++ Content/js/project/common/forms.js
@@ -131,12 +131,11 @@
 	}
 */
 define([
-	"common/model",
 	"common/services",  
 	"validate"
 ], 
 
-function (model, services) 
+function (services) 
 {
 	// ---------------------------------------------------------------
 	//
@@ -150,7 +149,6 @@ function (model, services)
 		var self = this;
 
 		// core objects
-		this.oModel = model;  
 		this.oServices = services;
 
 		this.STR_ERROR_FIELDS       = "Please complete the fields highlighted above.";
